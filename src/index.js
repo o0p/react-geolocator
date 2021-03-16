d9596faed25f79c61b73123ea27816d5919f9884
o0p <rus@gmx.com>
2021-03-16 13:57:51 +0000

    Refactor state

--- src/index.js
+++ src/index.js
@@ -4,14 +4,7 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            lat: null,
-            errorMessage: ''
-        };
-    };
+    state = { lat: null, errorMessage: ''};
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(position => 
